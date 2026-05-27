2881a64254c550747c6cc6fd86030bb4563d8e8a
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-05-28 02:10:21 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,26 @@ import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
+//initialize dotenv
+dotenv.config();
+
 //start app
 const app = express();
-
-//routes for login,signup,logout
-app.use("/api/auth", authRoutes)
-
-// start server
-const PORT = process.env.PORT || 5000;
 
 // allows extraction of json data
 app.use(express.json());
 // allow to parse the cookie
 app.use(cookieParser());
 
+//routes for login,signup,logout
+app.use("/api/auth", authRoutes)
+
 //connect db
 connectDB();
 
-dotenv.config();
+
+// start server port
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
