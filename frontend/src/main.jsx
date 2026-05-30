14223a1b88932fdfec581faf7717290a5d87fa47
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-05-30 17:51:14 +0300

    added pages and components and store in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
 
 
-// to do list
+// to do listcd
 
 // install npm react router dom react hot toast initallize tailwind css npm install -D tailwindcss autoprefixer daisy ui go to daisy ui .com use import daisy ui
 //delete app.css
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <div>
+      sign up
+    
+    </div>
+  )
+}
+
+export default SignUpPage
+
+//install axios,zustand 2. install daisy ui 
