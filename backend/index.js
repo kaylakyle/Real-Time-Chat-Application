beecb0bced88ae1bf51094069663b3fd29730660
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-06-16 00:33:04 +0300

    added socket.js

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ import cors from "cors";
 
 import path from "path";
 
-import { connectDB } from "./lib/db.js";
+import  connectDB  from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
