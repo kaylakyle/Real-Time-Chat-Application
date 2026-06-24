1c92bed162f8a290a5b951191a0049ef6f8f00d2
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-06-24 07:08:51 +0300

    Fix wildcard route syntax: changed * to /*

--- backend/index.js
+++ backend/index.js
@@ -1,80 +1,3 @@
-// import express from "express";
-// import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
-// import dotenv from "dotenv";
-// import connectDB from "./lib/db.js";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import http from "http";
-// import { Server } from "socket.io";
-
-// dotenv.config();
-
-// const app = express();
-
-// // MIDDLEWARE
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true,
-// }));
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // ROUTES
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-
-
-// // DB
-// connectDB();
-
-
-// // SOCKET SETUP 
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   },
-// });
-
-// // store online users
-// const userSocketMap = {};
-
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   const userId = socket.handshake.query.userId;
-
-//   if (userId) {
-//     userSocketMap[userId] = socket.id;
-//   }
-
-//   // send online users to all clients
-//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-
-//     if (userId) {
-//       delete userSocketMap[userId];
-//     }
-
-//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-//   });
-// });
-
-// // START SERVER
-// const PORT = process.env.PORT || 5000;
-
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -82,7 +5,7 @@ import cors from "cors";
 
 import path from "path";
 
-import  connectDB  from "./lib/db.js";
+import connectDB from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -108,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
